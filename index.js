920e5fab2474f8d42e72fdbf46fded1ccc919b89
Atef <mahmoudbenatef@gmail.com>
2021-07-01 11:11:36 +0200

    solve sockets problems

--- index.js
+++ index.js
@@ -5,29 +5,37 @@ const io = require("socket.io")(8900, {
 })
 let users = []
 const addUser = (userId, socketId) => {
-    !users.some(user => user.userId === userId) &&
+    if (users.filter(user => user.userId === userId).length === 0) {
+        console.log("new user added")
         users.push({ userId, socketId })
+    }
+    else {
+        // users.filter(user => user.userId === userId)[0].socketId = socketId;
+    }
     console.log(users);
 }
 const removeUser = (socketId) => {
     users = users.filter(user => user.socketId !== socketId)
 }
 io.on("connection", (socket) => {
-    console.log(" a user connected");
+    // console.log(" a user connected");
     // io.emit("welcome", "hello this is socket server ")
     socket.on("addUser", userId => {
+        console.log("adding new one ");
         addUser(userId, socket.id)
     })
     io.emit("getUsers", users)
     socket.on("disconnect", () => {
         removeUser(socket.id);
     })
-    socket.on("newMessage", userId => {
+    socket.on("newMessage", (userId) => {
         console.log(`New message there to ${userId}`);
         let user = users.find(user => user.userId === userId)
-        console.log(users);
-        console.log(user);
-        if (user)
+        console.log("list of users")
+        console.log("user existed", user)
+        if (user) {
+            console.log("sending to user");
             io.to(user.socketId).emit("you got new Message");
+        }
     })
 })
